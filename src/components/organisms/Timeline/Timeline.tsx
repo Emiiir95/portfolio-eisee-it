8359520ce672b611c5fddae39ebefa2003121bbf
Emir Sen <emirsen@OrdinatledeEmir.lan>
2025-04-10 13:42:25 +0200

    build

--- src/components/organisms/Timeline/Timeline.tsx
+++ src/components/organisms/Timeline/Timeline.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Timeline as TimelineDemo } from "@/components/ui/timeline";
 import { Typography } from "@/components/atoms/Typography/Typography";
+import snowpact from "@/assets/images/timeline/snowpact.png";
+import helio from "@/assets/images/timeline/helio.png";
+import itstock from "@/assets/images/timeline/itstock.png";
+import signaleo from "@/assets/images/timeline/signaleo.png";
 
 export function Timeline() {
   const data = [
@@ -16,28 +20,24 @@ export function Timeline() {
           </Typography>
           <div className="grid grid-cols-2 gap-4">
             <img
-              src="https://assets.aceternity.com/templates/startup-1.webp"
+              src={snowpact}
               alt="startup template"
-              width={500}
-              height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <img
-              src="https://assets.aceternity.com/templates/startup-2.webp"
+              src={itstock}
               alt="startup template"
-              width={500}
-              height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <img
-              src="https://assets.aceternity.com/templates/startup-3.webp"
+              src={signaleo}
               alt="startup template"
               width={500}
               height={500}
-              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+              className="rounded-lg h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <img
-              src="https://assets.aceternity.com/templates/startup-4.webp"
+              src={helio}
               alt="startup template"
               width={500}
               height={500}
